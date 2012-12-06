1fb85ff1033af259d4a76e8472571c7d07156ab0
404 <404@example.com>
2012-12-06 11:45:54 +0800

    重新写了remove-linefeed-in-paragraph；修复之前错误，增加测试，并且让帮助函数和测试都在主脚本中
    这样方便直接给别人使用本工具
    
    本工具要做的是修改一类文本，比如gutenburg的txt小说。它设定了每行最大字数。
    
    我们需要让该文本更像markdwon格式。也就是段落之间猜有换行。同一段落的不要换行。
    
    对于那些开头有空白的行，类似诗歌的问题，我们认为它相当与markdwon的pre语法，不做任何修饰
    
    UNFORMATED TEXT
    
    1111111
    1111111
    
      2222
      2222
    
    1111111
    1111
    1111
    
    要修改成
    
    FORMATED TEXT
    
    1111111 11111
    
       2222
       2222
    
    11111 1111 1111

--- remove-linefeed-in-paragraph.rb
+++ remove-linefeed-in-paragraph.rb
@@ -1,24 +1,22 @@
 # -*- coding: utf-8 -*-
-require './lib/class.rb'
+# require './lib/class.rb'
 require 'find'
 require 'fileutils'
+require 'minitest/autorun'
+require 'pp'
+
+# usage: ruby -w script.rb inputfolder outputfolder
+
 $input = File.expand_path(ARGV[0])
 $output = File.expand_path(ARGV[1])
 
 def r_rm_line_feed(input, output)
-  # expand input out path first, or else
-  # e.g input is 'user', output if 'other'
-  # /home/user/user will be replaced as /home/other/user, wrong!
-  # should be /home/user/other ya
-  # if we expand the path, that won't happen
   input = File.expand_path $input
   output = File.expand_path $output 
-  
   Find.find(input) do |file|
     next unless file =~ /txt$/i
     next if File.directory?(file)
-#    $inputfile = File.basename(file, 'txt') # $inputfile is filename. , with a dot
-    $inputfile = File.basename(file) # $inputfile is filename. , with a dot
+    $inputfile = File.basename(file) # $inputfile is filename with suffix
     $inputpath = File.dirname(file)
     $newpath = $inputpath.sub(input, output)
     FileUtils.mkdir_p $newpath unless File.exist? $newpath
@@ -30,37 +28,42 @@ def r_rm_line_feed(input, output)
       f.puts newtext
     end
   end
-  ending_msg
+  puts "\nYour documents are ready at #{output}!\n\n\n"
 end
 r_rm_line_feed($input, $output)
 
-str = '''
-Splits str using the supplied parameter as the
-record separator ($/ by default), passing each 
-substring in turn to the supplied block. If a
-zero-length record separator is supplied, the
-string is split into paragraphs delimited by multiple
-successive newlines.
+class String
+  def remove_line_feed
+    text = 
+      self.sub(/^\xef\xbb\xbf/, '') # remove bom
+      .gsub(/\r/, '')
+      .gsub(/\n\n+/, "\n\n")
+    arr = text.split("\n\n")
+    arr.map! do |line|
+      if line =~ /^\s/ #开头是空白的行不处理 
+        line
+      else
+        line = line.gsub(/\n/, ' ')
+      end
+    end
+    # p arr
+    arr.join("\n\n")
+  end
+end
 
+# tests
 
-  poem goes from here
-  blue fish, red fish
-  cool fish, snoop fish
+UNFORMATED = "111111\n111111\n\n\n   22222\n   22222\n\n\n\n111111\n111111"
+FORMATED   = "111111 111111\n\n   22222\n   22222\n\n111111 111111"
 
+class TestSanitize < MiniTest::Unit::TestCase
 
-string is split into paragraphs delimited by multiple
-string is split into paragraphs delimited by multiple
-string is split into paragraphs delimited by multiple
+  def test_remove_line_feed
+    assert_equal "111 111\n\n111 111", "111\n111\n\n\n111\n111".remove_line_feed, 'join lines'
+    assert_equal "111 111\n\n111 111\n\n222 222 ", "111\n111\n\n\n111\n111\n\n\n222\n222\n".remove_line_feed, 'two, paragraphs, join lines'
+    assert_equal " 222\n 222\n 222", " 222\n 222\n 222".remove_line_feed, 'do not join lines pre lines'
+    assert_equal " 222\n 222\n\n 222\n 222", " 222\n 222\n\n\n\n\n 222\n 222".remove_line_feed, 'two parts, do not join lines pre lines'
+    assert_equal FORMATED, UNFORMATED.remove_line_feed, 'mix paragraph and pre'
+  end
 
-record separator ($/ by default), passing each 
-substring in turn to the supplied block. If a
-zero-length record separator is supplied, the
-string is split into paragraphs delimited by multiple
-successive newlines.
-
-
-
-'''
-File.open('t.txt', 'w') do |f|
-  f.puts str.remove_line_feed
 end
